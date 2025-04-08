1f2362b535a928ea678cc4319d55a122c9c11c3e
sivaram000 <katta.sivaram@suprsend.com>
2025-04-08 16:22:18 +0530

    added arabic language

---
+++ src/i18n/languages/ar.ts
@@ -0,0 +1,18 @@
+import { ILanguage } from '../../interface';
+
+const ar: ILanguage = {
+  language: 'ar',
+  translations: {
+    notifications: 'إشعارات',
+    markAllAsRead: 'وضع علامة على الكل كمقروءة',
+    noNotificationsTitle: 'لا توجد إخطارات حتى الآن',
+    noNotificationsDescription: 'سنخبرك عندما نحصل على شيء جديد لك.',
+    pinned: 'مثبت',
+    markAsUnread: 'وضع علامة كغير مقروءة',
+    markAsRead: 'وضع علامة كمقروءة',
+    archive: 'أرشيف',
+    expiresIn: 'تنتهي في',
+  },
+};
+
+export default ar;
